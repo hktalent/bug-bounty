69675e575ed9fa71793eea6d51c47d1e32e8482b
hktalent <18223385+hktalent@users.noreply.github.com>
2023-09-30 12:51:45 +0800

    add labs 2023-09-30

---
+++ labs/CVE-2019-11043/php/index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Exploit PHP-FPM as proposed in CVE-2019-11043</title>
+ </head>
+ <body>
+     <?php echo '<p>Challenge PHP with nginx + php-fpm configurations.</p>';?>
+ </body>
+</html>
